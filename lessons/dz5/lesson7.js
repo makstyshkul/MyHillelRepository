cbe90cbc7d5803b4c506b851147a20f6dda5a613
Max Tyshkyl <maxt2004@ukr.net>
2022-10-28 12:26:53 +0300

    lesson7

--- lessons/dz5/lesson7.js
+++ lessons/dz5/lesson7.js
@@ -1,55 +1,52 @@
-class Apartments{
-constructor(floats,floors,number){
-this.floats = floats;
-this.floors = floors;
-this.number = number;
-}
-float(){
-	this.person = float.person;
-	this.floor =  float.floor;
-	this.square =  float.square;
-}
-}
-class AddNewApartment{
-	floats = 0;
-	constructor(count){
-		this.floats = this.floats + count;
-		this.floors = apartment.floors;
-this.number = apartment.number;
+class Apartments {
+	constructor(flats, floors, number) {
+		this.flats = flats;
+		this.floors = floors;
+		this.number = number;
+	}
+	// flat(){
+	// 	this.person = flat.person;
+	// 	this.floor =  flat.floor;
+	// 	this.square =  flat.square;
+	// }
+	AddNewApartment(countFlats, countFloors, number) {
+		this.flats = countFlats;
+		this.floors = countFloors;
+		this.number = number;
 	}
 }
 
-class Float{
-constructor(person,floor,square){
-	this.person = person;
-	this.floor = floor;
-	this.square =square;
-}
-persons(){
-	this.surname = persons.surname;
-	this.name = persons.name;
-	this.age = persons.age;
-	this.married = persons.married;
-	this.child = persons.child;
+class Flat {
+	constructor(person, floor, square) {
+		this.person = person;
+		this.floor = floor;
+		this.square = square;
+	}
+	persons() {
+		this.surname = persons.surname;
+		this.name = persons.name;
+		this.age = persons.age;
+		this.married = persons.married;
+		this.child = persons.child;
 	}
 }
 
-class Persons{
-constructor(surname,name,age,married,child){
-	this.surname = surname;
-this.name = name;
-this.age = age;
-this.married = married;
-this.child = child;
+class Persons {
+	constructor(surname, name, age, married, child) {
+		this.surname = surname;
+		this.name = name;
+		this.age = age;
+		this.married = married;
+		this.child = child;
+	}
 }
-}
-const persons = new Persons('Ivanov','Ivan',21,true,1);
-const float = new Float(3,2,70);
-const apartment = new Apartments(10,2,1)
-float.persons();
-console.log(float)
+const persons = new Persons('Ivanov', 'Ivan', 21, true, 1);
+const flat = new Flat(3, 2, 70);
+const apartment = new Apartments(10, 2, 1)
+flat.persons();
+console.log(flat)
 console.log(persons)
-apartment.float();
+// apartment.flat();
 console.log(apartment)
-const addNewApartment = new AddNewApartment(15);
-console.log(addNewApartment)
+apartment.AddNewApartment(15, 3, 2);
+console.log(apartment)
