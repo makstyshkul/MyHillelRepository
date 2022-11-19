e3d6f02daf3519efb92271e0fb41d322f71b8ac2
Max Tyshkyl <maxt2004@ukr.net>
2022-11-20 01:15:41 +0200

    lesson10

--- lessons/dz7(2)/lesson10.js
+++ lessons/dz7(2)/lesson10.js
@@ -5,7 +5,6 @@ const body = document.querySelector('body');
 	const array = ['<img src="images/1.jpg" alt="">','<img src="images/2.jpg" alt="">','<img src="images/3.jpg" alt="">','<img src="images/4.jpg" alt="">',
 	'<img src="images/5.jpg" alt="">','<img src="images/6.jpg" alt="">','<img src="images/7.jpg" alt="">','<img src="images/8.jpg" alt="">',
 	'<img src="images/9.jpg" alt="">'];
-	
 		let arrayItem = array[Math.floor(Math.random() * array.length)];
 		
 		body.innerHTML = arrayItem;
